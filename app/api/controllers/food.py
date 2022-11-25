dfc7b9297ebee25545a429000d20aab8c332e9e2
aguskertha <baguskertha@gmail.com>
2022-11-25 12:05:27 +0700

    feat(master): update model h5 and architecture

--- app/api/controllers/food.py
+++ app/api/controllers/food.py
@@ -99,13 +99,18 @@ def euclidean_distance(vects):
 def siamese_architecture():
     input = keras.layers.Input((224, 224, 3))
     x = keras.layers.BatchNormalization()(input)
-    x = keras.layers.Conv2D(64, (7, 7), activation="tanh")(x)
+    x = keras.layers.Conv2D(32, (3, 3), activation="tanh")(x)
+    x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
+    x = keras.layers.Conv2D(32, (5, 5), activation="tanh")(x)
+    x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
+    x = keras.layers.Conv2D(64, (5, 5), activation="tanh")(x)
     x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
     x = keras.layers.Conv2D(64, (7, 7), activation="tanh")(x)
     x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
-    x = keras.layers.Conv2D(64, (7, 7), activation="tanh")(x)
+    x = keras.layers.Conv2D(128, (7, 7), activation="tanh")(x)
     x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
 
+    x = keras.layers.Dropout(0.5)(x) 
     x = keras.layers.Flatten()(x)
 
     x = keras.layers.BatchNormalization()(x)

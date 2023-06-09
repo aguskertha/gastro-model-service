843b9ed95b72afbb05aa5198b07e0a5430aec1f9
aguskertha <baguskertha@gmail.com>
2023-06-09 14:54:52 +0700

    feat(mode): update model mobilenet

--- app/api/utils/architecture.py
+++ app/api/utils/architecture.py
@@ -18,44 +18,44 @@ def euclidean_distance(vects):
     return tf.math.sqrt(tf.math.maximum(sum_square, keras.backend.epsilon()))
 
 def siamese_architecture():
-    input = keras.layers.Input((224, 224, 3))
-    x = tf.keras.layers.BatchNormalization()(input)
-    x = keras.layers.Conv2D(32, (3, 3), activation="tanh")(x)
-    x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
-    x = keras.layers.Conv2D(32, (5, 5), activation="tanh")(x)
-    x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
-    x = keras.layers.Dropout(0.5)(x) 
-    x = keras.layers.Conv2D(64, (5, 5), activation="tanh")(x)
-    x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
-    x = keras.layers.Dropout(0.5)(x) 
-    x = keras.layers.Conv2D(64, (7, 7), activation="tanh")(x)
-    x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
-    x = keras.layers.Dropout(0.5)(x) 
-    x = keras.layers.Conv2D(128, (7, 7), activation="tanh")(x)
-    x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
-    x = keras.layers.Dropout(0.5)(x)
+    # input = keras.layers.Input((224, 224, 3))
+    # x = tf.keras.layers.BatchNormalization()(input)
+    # x = keras.layers.Conv2D(32, (3, 3), activation="tanh")(x)
+    # x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    # x = keras.layers.Conv2D(32, (5, 5), activation="tanh")(x)
+    # x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    # x = keras.layers.Dropout(0.5)(x) 
+    # x = keras.layers.Conv2D(64, (5, 5), activation="tanh")(x)
+    # x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    # x = keras.layers.Dropout(0.5)(x) 
+    # x = keras.layers.Conv2D(64, (7, 7), activation="tanh")(x)
+    # x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    # x = keras.layers.Dropout(0.5)(x) 
+    # x = keras.layers.Conv2D(128, (7, 7), activation="tanh")(x)
+    # x = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    # x = keras.layers.Dropout(0.5)(x)
 
-    x = keras.layers.Flatten()(x)
+    # x = keras.layers.Flatten()(x)
 
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = keras.layers.Dense(10, activation="tanh")(x) #10 num class
-    embedding_network = keras.Model(input, x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = keras.layers.Dense(10, activation="tanh")(x) #10 num class
+    # embedding_network = keras.Model(input, x)
 
-
-    input_1 = keras.layers.Input((224, 224, 3))
-    input_2 = keras.layers.Input((224, 224, 3))
-
-    # As mentioned above, Siamese Network share weights between
-    # tower networks (sister networks). To allow this, we will use
-    # same embedding network for both tower networks.
-    tower_1 = embedding_network(input_1)
-    tower_2 = embedding_network(input_2)
 
     # input_1 = keras.layers.Input((224, 224, 3))
     # input_2 = keras.layers.Input((224, 224, 3))
-    # mobilenet_model = MobileNetV2(weights='imagenet')
-    # tower_1 = mobilenet_model(input_1)
-    # tower_2 = mobilenet_model(input_2)
+
+    # # As mentioned above, Siamese Network share weights between
+    # # tower networks (sister networks). To allow this, we will use
+    # # same embedding network for both tower networks.
+    # tower_1 = embedding_network(input_1)
+    # tower_2 = embedding_network(input_2)
+
+    input_1 = keras.layers.Input((224, 224, 3))
+    input_2 = keras.layers.Input((224, 224, 3))
+    mobilenet_model = mobilenet(weights='imagenet')
+    tower_1 = mobilenet_model(input_1)
+    tower_2 = mobilenet_model(input_2)
 
     merge_layer = keras.layers.Lambda(euclidean_distance)([tower_1, tower_2])
     normal_layer = tf.keras.layers.BatchNormalization()(merge_layer)
